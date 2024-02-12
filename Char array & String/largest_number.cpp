32470f1303602bd723d3ce32c7d9b01b2c7766f3
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-12 13:59:43 +0530

    Create largest_number.cpp

---
+++ Char array & String/largest_number.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+using namespace std;
+
+// algorithm:
+// question: largest number
+// example:
+// answer:
+// explanation:
+// tags:
+
+static bool mycomp(string a,string b){
+    string t1 = a + b;
+    string t2 = b + a;
+    return t1 > t2;
+}
+
+string largestNumber(vector<int>& nums) {
+    vector<string>snums;
+    for(auto n : nums){
+        snums.push_back(to_string(n));
+    }
+    // Sorting according to comparator function
+    sort(snums.begin(),snums.end(),mycomp);
+    string ans = "";
+    for(int i = 0;i < snums.size();i++){
+        ans = ans + snums[i];
+    }
+    // (If first character is 0, this means largest number is 0 and hence all will be 0)
+    if(ans[0] == '0'){
+        ans = "0";
+    }
+    return ans;
+}
+
+int main ()
+{
+    vector<int> nums = {3, 30, 34, 5, 9};
+    string ans = largestNumber(nums);
+    cout<<ans<<endl;
+    return 0;
+}
