75e3fd7dff34598036942f7dba1a71eb6e7d9b6d
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-13 19:17:28 +0530

    Create fibonacci_series.cpp

---
+++ Recursion/fibonacci_series.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n)
+{
+    // Base case
+    if (n == 0)
+    {
+        // 0th term needed
+        return 0;
+    }
+    if (n == 1)
+    {
+        // 1st term needed
+        return 1;
+    }
+    // Recursive Relation
+    int ans = fib(n - 1) + fib(n - 2);
+    return ans;
+}
+
+int main()
+{
+    int n = 5; // 5th term of the series we want
+    int ans = fib(n);
+    cout << ans << endl;
+    return 0;
+}
