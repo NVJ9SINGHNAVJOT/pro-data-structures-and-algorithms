4e908c2880ca5cbd3cfd291c39d4275079850396
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-13 18:41:53 +0530

    Create insertion_sort.cpp

---
+++ Searching & Sorting/insertion_sort.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <numeric>
+using namespace std;
+
+// algorithm: insertion sort
+// question:
+// example:
+// answer:
+// explanation:
+// tags:
+
+void printArr(vector<int> arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+void insertionSort(vector<int> &arr)
+{
+    int n = arr.size();
+    for (int i = 1; i < n; i++)
+    { // number of rounds is n-1 for n elements
+        // Step - 1 Pick the element
+        int val = arr[i];
+        int j = i - 1; // So that we can access outside loop
+        for (; j >= 0; j--)
+        { // compare with all the elements behind the ith element
+            if (arr[j] > val)
+            {
+                // Do shifting operation
+                arr[j + 1] = arr[j];
+            }
+            else
+            {
+                break; // simply exit the loop
+            }
+        }
+        // Copying step
+        arr[j + 1] = val;
+    }
+}
+
+int main()
+{
+    vector<int> arr{10, 1, 4, 8, 5, 7};
+    cout << "Printing before sorted " << endl;
+    printArr(arr);
+    cout << "Printing array after sorting " << endl;
+    insertionSort(arr);
+    printArr(arr);
+    return 0;
+}
