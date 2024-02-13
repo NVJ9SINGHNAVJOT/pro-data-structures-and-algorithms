bd77d9915b06fbe3e2983c321a685c5924bd4090
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-13 13:42:56 +0530

    Create add_2_numbers_represented_by_linked_list.cpp

---
+++ Linked List/add_2_numbers_represented_by_linked_list.cpp
@@ -0,0 +1,318 @@
+#include <iostream>
+using namespace std;
+
+// algorithm:
+// question: add 2 numbers represented by linked list
+// example:
+// answer:
+// explanation:
+// tags:
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    // TODO: Write a destrcutor to delete a node
+    ~Node()
+    {
+
+        cout << "Node with value: " << this->data << " deleted" << endl;
+    }
+};
+
+// I want to insert a node right at the head of Linked List
+void insertAtHead(Node *&head, Node *&tail, int data)
+{
+    // check for Empty LL
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        // step1:
+        Node *newNode = new Node(data);
+        // step2:
+        newNode->next = head;
+        // step3:
+        head = newNode;
+    }
+}
+
+// I want to insert a node right at the end of LINKED LIST
+void insertAtTail(Node *&head, Node *&tail, int data)
+{
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    // step1: creatae a node
+    Node *newNode = new Node(data);
+    // step2: connect woth tail ndoe
+    tail->next = newNode;
+    // step3: update tail;
+    tail = newNode;
+}
+void print(Node *head)
+{
+
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int findLength(Node *&head)
+{
+    int len = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        len++;
+    }
+    return len;
+}
+
+void insertAtPosition(int data, int position, Node *&head, Node *&tail)
+{
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    // step1: find the position: prev & curr;
+
+    if (position == 0)
+    {
+        insertAtHead(head, tail, data);
+        return;
+    }
+
+    int len = findLength(head);
+
+    if (position >= len)
+    {
+        insertAtTail(head, tail, data);
+        return;
+    }
+    // ste1:find prev and curr
+    int i = 1;
+    Node *prev = head;
+    while (i < position)
+    {
+        prev = prev->next;
+        i++;
+    }
+    Node *curr = prev->next;
+
+    // step2;
+    Node *newNode = new Node(data);
+
+    // step3:
+    newNode->next = curr;
+
+    // step4:
+    prev->next = newNode;
+}
+
+void deleteNode(int position, Node *&head, Node *&tail)
+{
+    if (head == NULL)
+    {
+        cout << "Cannot delete, LL is empty";
+        return;
+    }
+
+    // deleting first node
+    if (position == 0)
+    {
+        Node *temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+        return;
+    }
+    int len = findLength(head);
+
+    // deleting last node
+    if (position == len - 1)
+    {
+        // find prev
+        int i = 1;
+        Node *prev = head;
+        while (i < position)
+        {
+            prev = prev->next;
+            i++;
+        }
+        // step2:
+        prev->next = NULL;
+        // step3:
+        Node *temp = tail;
+        // step4:
+        tail = prev;
+        // step5:
+        delete temp;
+        return;
+    }
+
+    // deleting middle node
+
+    // step  : find prev and curr
+    int i = 1;
+    Node *prev = head;
+    while (i < position)
+    {
+        prev = prev->next;
+        i++;
+    }
+    Node *curr = prev->next;
+
+    // step2:
+    prev->next = curr->next;
+    // step3:
+    curr->next = NULL;
+    // step4:
+    delete curr;
+}
+
+Node *reverse(Node *head)
+{
+    Node *prev = NULL;
+    Node *curr = head;
+    Node *next = curr->next;
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+Node *addTwoLL(Node *&head1, Node *&head2)
+{
+
+    if (head1 == NULL)
+        return head2;
+    if (head2 == NULL)
+        return head1;
+
+    // step1 : reverse both linked list
+    head1 = reverse(head1);
+    head2 = reverse(head2);
+
+    // step2: add both linked list
+    Node *ansHead = NULL;
+    Node *ansTail = NULL;
+    int carry = 0;
+
+    while (head1 != NULL && head2 != NULL)
+    {
+        // calculate sum
+        int sum = carry + head1->data + head2->data;
+        // find digit to create node for
+        int digit = sum % 10;
+        // calculate carry
+        carry = sum / 10;
+
+        // create a new Node for the digit
+        Node *newNode = new Node(digit);
+        // attach the newNode into the answer wali linked list
+        if (ansHead == NULL)
+        {
+            // first node insert krre ho
+            ansHead = newNode;
+            ansTail = newNode;
+        }
+        else
+        {
+            ansTail->next = newNode;
+            ansTail = newNode;
+        }
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+
+    // jab head1 list ki length head2 list se jada hogi
+    while (head1 != NULL)
+    {
+        int sum = carry + head1->data;
+        int digit = sum % 10;
+        carry = sum / 10;
+        Node *newNode = new Node(digit);
+        ansTail->next = newNode;
+        ansTail = newNode;
+        head1 = head1->next;
+    }
+
+    // jab head2 list ki length head1 list se jada hogi
+    while (head2 != NULL)
+    {
+        int sum = carry + head2->data;
+        int digit = sum % 10;
+        carry = sum / 10;
+        Node *newNode = new Node(digit);
+        ansTail->next = newNode;
+        ansTail = newNode;
+        head2 = head2->next;
+    }
+
+    // handle carry ko alag se
+    while (carry != 0)
+    {
+        int sum = carry;
+        int digit = sum % 10;
+        carry = sum / 10;
+        Node *newNode = new Node(digit);
+        ansTail->next = newNode;
+        ansTail = newNode;
+    }
+
+    // step3: reverse the ans linked list
+    ansHead = reverse(ansHead);
+    return ansHead;
+}
+
+int main()
+{
+    Node *head1 = new Node(9);
+    Node *second1 = new Node(9);
+    head1->next = second1;
+
+    Node *head2 = new Node(9);
+    Node *second2 = new Node(9);
+    // Node* third2 = new Node(4);
+    head2->next = second2;
+    // second2 -> next = third2;
+
+    Node *ans = addTwoLL(head1, head2);
+
+    print(ans);
+
+    return 0;
+}
