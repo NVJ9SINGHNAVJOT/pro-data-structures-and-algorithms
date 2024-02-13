95ace713914a98e7e31b2019a67f10dcaa73db8f
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-13 19:42:10 +0530

    Create climbing_stairs.cpp

---
+++ Recursion/climbing_stairs.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+using namespace std;
+
+// algorithm:
+// question: climbing stairs
+// example:
+// answer:
+// explanation:
+// tags:
+
+int climbStairs(int n)
+{
+    // Base Case
+    if (n == 0 || n == 1)
+    {
+        // 1 way to reach 0th and 1st stair
+        return 1;
+    }
+    // Recursive Relation
+    int ans = climbStairs(n - 1) + climbStairs(n - 2);
+    return ans;
+}
+int main()
+{
+    int n = 15;
+    int ways = climbStairs(n);
+    cout << "Total ways to reach the " << n << " stair is " << ways << endl;
+    return 0;
+}
