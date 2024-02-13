47359247cdb5d28222fdec0b5b77cf36f18008cd
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-13 21:56:56 +0530

    Create max_segment_of_rod.cpp

---
+++ Recursion/max_segment_of_rod.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+using namespace std;
+
+// algorithm:
+// question: max segment of rod
+// example:
+// answer:
+// explanation:
+// tags:
+
+int solveRec(vector<int> arr, int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    if (n < 0)
+    {
+        return INT_MIN;
+    }
+
+    int maxi = INT_MIN;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int ans = solveRec(arr, n - arr[i]);
+        maxi = max(maxi, ans + 1);
+    }
+
+    return maxi;
+}
+
+int main()
+{
+    vector<int> arr{3, 3, 3};
+    int n = 8;
+
+    int ans = solveRec(arr, n);
+    if (ans < 0)
+    {
+        ans = ans + INT_MAX;
+    }
+    cout << ans << endl;
+    return 0;
+}
