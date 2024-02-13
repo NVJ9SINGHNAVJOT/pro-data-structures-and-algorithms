fc87d035fe2ce0a80c454632db77e883cfa2871e
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-13 16:48:26 +0530

    Create valid_parenthesis.cpp

---
+++ Stack/valid_parenthesis.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+// algorithm:
+// question: valid parenthesis
+// example:
+// answer:
+// explanation:
+// tags:
+
+bool isValid(string s)
+{
+
+    stack<char> st;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        char ch = s[i];
+
+        // opening bracket
+        if (ch == '(' || ch == '{' || ch == '[')
+        {
+            st.push(ch);
+        }
+        else
+        {
+            // closing bracket - ch
+            if (!st.empty())
+            {
+                char topCh = st.top();
+                if (ch == ')' && topCh == '(')
+                {
+                    // matching brackets
+                    st.pop();
+                }
+                else if (ch == '}' && topCh == '{')
+                {
+                    // matching brackets
+                    st.pop();
+                }
+                else if (ch == ']' && topCh == '[')
+                {
+                    // matching brackets
+                    st.pop();
+                }
+                else
+                {
+                    // brackets not matching
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    if (st.empty())
+    {
+        // valid
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    string s = "(){}[]";
+    bool ans = isValid(s);
+    cout << ans << endl;
+    return 0;
+}
