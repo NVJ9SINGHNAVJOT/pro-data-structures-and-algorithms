4cac165080454ad47d12d27d51d858243a21c143
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-13 19:58:58 +0530

    Create subsequence_sets.cpp

---
+++ Recursion/subsequence_sets.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+using namespace std;
+
+// algorithm:
+// question: subsequence sets
+// example:
+// answer:
+// explanation:
+// tags:
+
+void printSubSequence(string str, string output, int i)
+{
+    // Base Case
+    if (i >= str.length())
+    {
+        // print and then return
+        cout << output << endl;
+        return;
+    }
+
+    // Call for exclude (As it is)          exclude
+    printSubSequence(str, output, i + 1);
+    
+    // Call for include (Concatenate)       include
+    output.push_back(str[i]);
+    printSubSequence(str, output, i + 1);
+}
+
+int main()
+{
+    string str = "abc";
+    string output = ""; // Initially empty
+    int i = 0;
+    printSubSequence(str, output, i);
+    return 0;
+}
