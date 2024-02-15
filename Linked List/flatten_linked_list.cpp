178e10111df77b0a0a51f92c4506da368f5b1d3e
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-15 18:30:00 +0530

    Create flatten_linked_list.cpp

---
+++ Linked List/flatten_linked_list.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+// algorithm:
+// question: flatten linked list
+// example:
+// answer:
+// explanation:
+// tags:
+
+Node *merge(Node *a, Node *b)
+{
+    if (!a)
+        return b;
+    if (!b)
+        return a;
+
+    Node *ans = 0;
+
+    if (a->data <= b->data)
+    {
+        ans = a;
+        a->bottom = merge(a->bottom, b);
+    }
+    else
+    {
+        ans = b;
+        b->bottom = merge(a, b->bottom);
+    }
+    return ans;
+}
+
+Node *flatten(Node *root)
+{
+    if (!root)
+        return 0;
+    Node *mergedLL = merge(root, flatten(root->next));
+    return mergedLL;
+}
