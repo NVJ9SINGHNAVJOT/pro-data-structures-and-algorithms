73a4e49fddb4f75cb0034bc975d22c4f460188e6
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-16 17:37:40 +0530

    Create longest_valid_parentheses.cpp

---
+++ Stack/longest_valid_parentheses.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+// algorithm:
+// question: longest valid parentheses
+// example:
+// answer:
+// explanation:
+// tags:
+
+int longestValidParentheses(string s)
+{
+    stack<int> st;
+    st.push(-1);
+    int maxLen = 0;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        char ch = s[i];
+        if (ch == '(')
+        {
+            st.push(i);
+        }
+        else
+        {
+            st.pop();
+            if (st.empty())
+            {
+                st.push(i);
+            }
+            else
+            {
+                int len = i - st.top();
+                maxLen = max(len, maxLen);
+            }
+        }
+    }
+    return maxLen;
+}
+
+int main()
+{
+    string s = ")()())";
+    int ans = longestValidParentheses(s);
+    cout << ans << endl;
+    return 0;
+}
