f3186aaccabb52a825c2e0e4c07331fe96992fe3
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-16 17:49:21 +0530

    Create next_smallest_element.cpp

---
+++ Stack/next_smallest_element.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+// algorithm:
+// question: next smallest element
+// example:
+// answer:
+// explanation:
+// tags:
+
+vector<int> nextSmaller(vector<int> &input)
+{
+    stack<int> s;
+    s.push(-1);
+
+    vector<int> ans(input.size());
+
+    for (int i = input.size() - 1; i >= 0; i--)
+    {
+        int curr = input[i];
+
+        // apka answer stack me
+        while (s.top() >= curr)
+        {
+            s.pop();
+        }
+
+        // chotta element mil chuka h -> ans store
+        ans[i] = s.top();
+
+        // push krdo curr element ko
+        s.push(curr);
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> input;
+    input.push_back(2);
+    input.push_back(1);
+    input.push_back(4);
+    input.push_back(3);
+
+    vector<int> ans = nextSmaller(input);
+
+    cout << "Printing ans1" << endl;
+
+    for (int i = 0; i < input.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
