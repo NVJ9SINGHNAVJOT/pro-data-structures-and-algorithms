9d278b1b346bd681d6255c422e0c6171b08d6d93
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-16 11:39:27 +0530

    Create rotate_linked_list.cpp

---
+++ Linked List/rotate_linked_list.cpp
@@ -0,0 +1,273 @@
+#include <iostream>
+using namespace std;
+
+// algorithm:
+// question: rotate linked list
+// example:
+// answer:
+// explanation:
+// tags:
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    // TODO: Write a destrcutor to delete a node
+    ~Node()
+    {
+
+        cout << "Node with value: " << this->data << " deleted" << endl;
+    }
+};
+
+// I want to insert a node right at the head of Linked List
+void insertAtHead(Node *&head, Node *&tail, int data)
+{
+    // check for Empty LL
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        // step1:
+        Node *newNode = new Node(data);
+        // step2:
+        newNode->next = head;
+        // step3:
+        head = newNode;
+    }
+}
+
+// I want to insert a node right at the end of LINKED LIST
+void insertAtTail(Node *&head, Node *&tail, int data)
+{
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    // step1: creatae a node
+    Node *newNode = new Node(data);
+    // step2: connect woth tail ndoe
+    tail->next = newNode;
+    // step3: update tail;
+    tail = newNode;
+}
+
+void print(Node *head)
+{
+
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int findLength(Node *&head)
+{
+    int len = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        len++;
+    }
+    return len;
+}
+
+void insertAtPosition(int data, int position, Node *&head, Node *&tail)
+{
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    // step1: find the position: prev & curr;
+
+    if (position == 0)
+    {
+        insertAtHead(head, tail, data);
+        return;
+    }
+
+    int len = findLength(head);
+
+    if (position >= len)
+    {
+        insertAtTail(head, tail, data);
+        return;
+    }
+    // ste1:find prev and curr
+    int i = 1;
+    Node *prev = head;
+    while (i < position)
+    {
+        prev = prev->next;
+        i++;
+    }
+    Node *curr = prev->next;
+
+    // step2;
+    Node *newNode = new Node(data);
+
+    // step3:
+    newNode->next = curr;
+
+    // step4:
+    prev->next = newNode;
+}
+
+void deleteNode(int position, Node *&head, Node *&tail)
+{
+    if (head == NULL)
+    {
+        cout << "Cannot delete, LL is empty";
+        return;
+    }
+
+    // deleting first node
+    if (position == 0)
+    {
+        Node *temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+        return;
+    }
+    int len = findLength(head);
+
+    // deleting last node
+    if (position == len - 1)
+    {
+        // find prev
+        int i = 1;
+        Node *prev = head;
+        while (i < position)
+        {
+            prev = prev->next;
+            i++;
+        }
+        // step2:
+        prev->next = NULL;
+        // step3:
+        Node *temp = tail;
+        // step4:
+        tail = prev;
+        // step5:
+        delete temp;
+        return;
+    }
+
+    // deleting middle node
+
+    // step  : find prev and curr
+    int i = 1;
+    Node *prev = head;
+    while (i < position)
+    {
+        prev = prev->next;
+        i++;
+    }
+    Node *curr = prev->next;
+
+    // step2:
+    prev->next = curr->next;
+    // step3:
+    curr->next = NULL;
+    // step4:
+    delete curr;
+}
+
+Node *rotateList(Node *head, int k)
+{
+    if (!head)
+        return 0;
+
+    int len = findLength(head);
+    int actualRotateK = (k % len);
+
+    if (actualRotateK == 0)
+        return head;
+
+    int newLastNodePos = len - actualRotateK - 1;
+    Node *newLastNode = head;
+    for (int i = 0; i < newLastNodePos; i++)
+
+    {
+        newLastNode = newLastNode->next;
+    }
+
+    Node *newHead = newLastNode->next;
+    newLastNode->next = 0;
+
+    // connecting last node to first node
+    Node *it = newHead;
+    while (it->next)
+    {
+        it = it->next;
+    }
+
+    it->next = head;
+
+    return newHead;
+}
+
+int main()
+{
+
+    Node *head = NULL;
+    Node *tail = NULL;
+    insertAtTail(head, tail, 20);
+    insertAtTail(head, tail, 50);
+    insertAtTail(head, tail, 60);
+    insertAtTail(head, tail, 90);
+    insertAtTail(head, tail, 77);
+
+    print(head);
+    cout << endl;
+    // cout << "head: " << head -> data << endl;
+    // cout << "tail: " << tail->data << endl;
+
+    // insertAtPosition(101, 5, head, tail);
+    // cout<< "Printing after insert at position call" << endl;
+    // print(head);
+    // cout << endl;
+    // cout << "head: " << head -> data << endl;
+    // cout << "tail: " << tail->data << endl;
+    insertAtPosition(101, 4, head, tail);
+    print(head);
+    cout << endl;
+    deleteNode(4, head, tail);
+    cout << endl;
+    print(head);
+    cout << endl;
+
+    head = rotateList(head, 3);
+
+    print(head);
+    cout << endl;
+
+    return 0;
+}
