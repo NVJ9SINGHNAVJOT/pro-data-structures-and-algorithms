35c90ff01ae5d6e70033c7003127d8a9f8e6b6ef
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-16 18:52:22 +0530

    Create count_the_reversals.cpp

---
+++ Stack/count_the_reversals.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+// algorithm:
+// question: count the reversals
+// example:
+// answer:
+// explanation:
+// tags:
+
+int countRev(string s)
+{
+    // if odd sized string impossibel to make pairs
+    if (s.size() & 1)
+        return -1;
+
+    int ans = 0;
+    stack<char> st;
+
+    for (auto ch : s)
+    {
+        if (ch == '{')
+        {
+            st.push(ch);
+        }
+        else
+        {
+            if (!st.empty() && st.top() == '{')
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(ch);
+            }
+        }
+    }
+
+    // if stack is not empty, let's count reversals
+    while (!st.empty())
+    {
+        char a = st.top();
+        st.pop();
+        char b = st.top();
+        st.pop();
+
+        if (a == b)
+        {
+            ans += 1;
+        }
+        else
+        {
+            ans += 2;
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    string s = "}{{}}{{{";
+    int ans = countRev(s);
+    cout << ans << endl;
+    return 0;
+}
