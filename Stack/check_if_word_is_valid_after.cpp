dfb7c74170d3badb60823c83132e7885c6f66eb8
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-17 15:57:00 +0530

    Create check_if_word_is_valid_after.cpp

---
+++ Stack/check_if_word_is_valid_after.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+// algorithm:
+// question: check if word is valid after
+// example:
+// answer:
+// explanation:
+// tags:
+
+bool isValid(string s)
+{
+    if (s[0] != 'a')
+    {
+        return false;
+    }
+
+    stack<char> st;
+
+    for (char ch : s)
+    {
+        if (ch == 'a')
+        {
+            st.push(ch);
+        }
+        else if (ch == 'b')
+        {
+            if (!st.empty() && st.top() == 'a')
+            {
+                st.push(ch);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            // ch == 'c'
+            if (!st.empty() && st.top() == 'b')
+            {
+                st.pop();
+                if (!st.empty() && st.top() == 'a')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    return st.empty();
+}
+
+int main()
+{
+    string s = "abcabcababcc";
+    bool ans = isValid(s);
+    cout << ans << endl;
+    return 0;
+}
