0c821501c8742e1f36096dd208b21f79b95acd0c
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-22 22:34:22 +0530

    Create reverse_k_elements_of_queue.cpp

---
+++ Queue/reverse_k_elements_of_queue.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+using namespace std;
+
+// algorithm:
+// question: reverse k elements of queue
+// example:
+// answer:
+// explanation:
+// tags:
+
+void reverseKElements(queue<int> &q, int k)
+{
+    // StepA: queue ->k elements  -> stack
+
+    stack<int> s;
+    int count = 0;
+    int n = q.size();
+
+    if (k <= 0 || k > n)
+        return;
+
+    while (!q.empty())
+    {
+        int temp = q.front();
+        q.pop();
+        s.push(temp);
+        count++;
+
+        if (count == k)
+            break;
+    }
+
+    // stepB: stack -> q me qapas
+    while (!s.empty())
+    {
+        int temp = s.top();
+        s.pop();
+        q.push(temp);
+    }
+
+    // step C: push n-k element from q front to back
+    count = 0;
+    while (!q.empty() && n - k != 0)
+    {
+        int temp = q.front();
+        q.pop();
+        q.push(temp);
+        count++;
+
+        if (count == n - k)
+            break;
+    }
+}
+
+int main()
+{
+    queue<int> q;
+
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+
+    reverseKElements(q, 3);
+    while (!q.empty())
+    {
+        cout << q.front() << ", ";
+        q.pop();
+    }
+
+    cout << endl;
+    return 0;
+}
