9bcf3514059b50bb7b6bb722ce8048d309f2049d
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-22 16:20:35 +0530

    Updated

---
+++ Queue/create_circular_queue.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+class CirQueue
+{
+public:
+    int size;
+    int *arr;
+    int front;
+    int rear;
+
+    CirQueue(int size)
+    {
+        this->size = size;
+        arr = new int[size];
+        front = -1;
+        rear = -1;
+    }
+
+    void push(int data)
+    {
+        // Queue Full
+
+        // single element case - > first element
+
+        // circular nature
+
+        // normal flow
+        // TODO: add one more condition in the QUEUE FULL if block
+        if ((front == 0 && rear == size - 1))
+        {
+            cout << "Q is fulll, cannot insert" << endl;
+        }
+        else if (front == -1)
+        {
+            front = rear = 0;
+            arr[rear] = data;
+        }
+        else if (rear == size - 1 && front != 0)
+        {
+            rear = 0;
+            arr[rear] = data;
+        }
+        else
+        {
+            rear++;
+            arr[rear] = data;
+        }
+    }
+
+    void pop()
+    {
+        // empty check
+        // single element
+        // circular nature
+        // normal flow
+        if (front == -1)
+        {
+            cout << "Q is empty , cannot pop" << endl;
+        }
+        else if (front == rear)
+        {
+            arr[front] = -1;
+            front = -1;
+            rear = -1;
+        }
+        else if (front == size - 1)
+        {
+            front = 0;
+        }
+        else
+        {
+            front++;
+        }
+    }
+};
+
+int main()
+{
+    CirQueue q(5);
+
+    q.push(5);
+    q.push(15);
+    q.push(25);
+    q.push(55);
+    q.push(120);
+    q.push(150);
+
+    return 0;
+}
---
+++ Queue/reverse_queue.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+using namespace std;
+
+// algorithm:
+// question: reverse queue
+// example:
+// answer:
+// explanation:
+// tags:
+
+void reverseQueue(queue<int> &q)
+{
+    stack<int> s;
+
+    // step 1: put all elements of q into s
+    while (!q.empty())
+    {
+        int element = q.front();
+        q.pop();
+
+        s.push(element);
+    }
+
+    // step2: put all element from stackinto q
+    while (!s.empty())
+    {
+        int element = s.top();
+        s.pop();
+
+        q.push(element);
+    }
+}
+
+void reverseQueueRecursion(queue<int> &q)
+{
+    // Base case
+    if (q.empty())
+        return;
+
+    // step A
+    int temp = q.front();
+    q.pop();
+
+    // step B
+    reverseQueueRecursion(q);
+
+    // step C;
+    q.push(temp);
+}
+
+int main()
+{
+    queue<int> q;
+
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+
+    reverseQueueRecursion(q);
+    return 0;
+}
