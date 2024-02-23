bd4297766d4db8947d1fbee007e486569f93772e
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-23 11:36:58 +0530

    Create queue_using_stacks.cpp

---
+++ Queue/queue_using_stacks.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+using namespace std;
+
+// algorithm:
+// question: queue using stacks
+// example:
+// answer:
+// explanation:
+// tags:
+
+class MyQueue
+{
+public:
+    stack<int> s1, s2;
+    MyQueue()
+    {
+    }
+
+    void push(int x)
+    {
+        s1.push(x);
+    }
+
+    int pop()
+    {
+        int pop = -1;
+        if (!s2.empty())
+        {
+            pop = s2.top();
+        }
+        else
+        {
+            while (!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+            pop = s2.top();
+        }
+        s2.pop();
+        return pop;
+    }
+
+    int peek()
+    {
+        int front = -1;
+        if (!s2.empty())
+        {
+            front = s2.top();
+        }
+        else
+        {
+            while (!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+            front = s2.top();
+        }
+        return front;
+    }
+
+    bool empty()
+    {
+        return s1.empty() && s2.empty();
+    }
+};
+
+int main()
+{
+    MyQueue q;
+
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+
+    cout << q.pop() << endl;
+    cout << q.peek() << endl;
+    cout << q.pop() << endl;
+    cout << q.peek() << endl;
+    return 0;
+}
