769e4504aad09cab1806189e6f520a12e30e1865
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-24 21:18:38 +0530

    Update generate_parentheses.cpp

--- Divide & Conquer and Backtracking/generate_parentheses.cpp
+++ Divide & Conquer and Backtracking/generate_parentheses.cpp
@@ -14,49 +14,47 @@ using namespace std;
 // explanation:
 // tags:
 
-void solve(vector<string> &ans, int n, int used_open, int used_close, int rem_open, int rem_close, string output)
+void solve(vector<string> &ans, int open, int close, string output)
 {
     // base case
-    if (rem_open == 0 && rem_close == 0)
+    if (open == 0 && close == 0)
     {
         ans.push_back(output);
         return;
     }
 
     // include open bracket
-    if (rem_open > 0)
+    if (open > 0)
     {
         // output.push_back('(');
-        solve(ans, n, used_open + 1, used_close, rem_open - 1, rem_close, output + '(');
+        solve(ans, open - 1, close, output + '(');
         // backtrack
-        //  output.pop_back();
+        // output.pop_back();
     }
 
     // include close bracket
-    if (used_open > used_close)
+    if (open < close)
     {
         // output.push_back(')');
-        solve(ans, n, used_open, used_close + 1, rem_open, rem_close - 1, output + ')');
+        solve(ans, open, close - 1, output + ')');
         // backtrack
-        //  output.pop_back();
+        // output.pop_back();
     }
 }
 
 vector<string> generateParenthesis(int n)
 {
     vector<string> ans;
-    int used_open = 0;
-    int used_close = 0;
-    int rem_open = n;
-    int rem_close = n;
+    int open = n;
+    int close = n;
     string output = "";
-    solve(ans, n, used_open, used_close, rem_open, rem_close, output);
+    solve(ans, open, close, output);
     return ans;
 }
 
 int main()
 {
-    int n = 3;
+    int n = 2;
     vector<string> ans = generateParenthesis(n);
 
     for (auto s : ans)
