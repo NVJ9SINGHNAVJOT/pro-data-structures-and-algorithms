d6f8266df4fd5a6aa6cd075a1d59d8a81547a3f0
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-26 19:53:12 +0530

    Create maximum_subarray.cpp

---
+++ Divide & Conquer and Backtracking/maximum_subarray.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+using namespace std;
+
+// algorithm: kadane's algorithm
+// question: maximum subarray
+// example:
+// answer:
+// explanation:
+// tags:
+
+int maxSumArrayHelper(vector<int> &v, int start, int end)
+{
+    if (start >= end)
+    {
+        return v[start];
+    }
+
+    int maxLeftBorderSum = INT_MIN, maxRightBorderSum = INT_MIN;
+    int mid = start + ((end - start) >> 1);
+
+    int maxLeftSum = maxSumArrayHelper(v, start, mid);
+    int maxRightSum = maxSumArrayHelper(v, mid + 1, end);
+
+    // max gross border sum
+    int leftBorderSum = 0, rightBorderSum = 0;
+    for (int i = mid; i >= 0; i--)
+    {
+        leftBorderSum += v[i];
+        if (leftBorderSum > maxLeftBorderSum)
+        {
+            maxLeftBorderSum = leftBorderSum;
+        }
+    }
+
+    for (int i = mid + 1; i <= end; i++)
+    {
+        rightBorderSum += v[i];
+        if (rightBorderSum > maxRightBorderSum)
+        {
+            maxRightBorderSum = rightBorderSum;
+        }
+    }
+
+    int crossBorderSum = maxLeftBorderSum + maxRightBorderSum;
+
+    return max(crossBorderSum, max(maxLeftSum, maxRightSum));
+}
+
+int maxSubArray(vector<int> &nums)
+{
+    return maxSumArrayHelper(nums, 0, nums.size() - 1);
+}
+
+int maxSubArray2(vector<int> &nums)
+{
+    int n = nums.size();
+
+    int maxSum = nums[0];
+    int currSum = nums[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (currSum > 0)
+        {
+            currSum = currSum + nums[i];
+        }
+        else
+            currSum = nums[i];
+
+        maxSum = max(maxSum, currSum);
+    }
+    return maxSum;
+}
+int main()
+{
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int ans = maxSubArray2(nums);
+    cout << ans << endl;
+    return 0;
+}
