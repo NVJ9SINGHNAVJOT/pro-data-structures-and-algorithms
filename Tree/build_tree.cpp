cf9307120a345e1c75a1fa820460defbf622843a
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-27 21:49:51 +0530

    Create build_tree.cpp

---
+++ Tree/build_tree.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node *buildTree()
+{
+    int data;
+    cout << "Enter the data: " << endl;
+    cin >> data;
+
+    if (data == -1)
+    {
+        return NULL;
+    }
+
+    // Step 1:
+    Node *root = new Node(data);
+
+    // step 2:
+    cout << "Enter data for left part of " << data << " node " << endl;
+    root->left = buildTree();
+
+    // step 3:
+    cout << "Enter data for right part of " << data << " node " << endl;
+    root->right = buildTree();
+
+    return root;
+}
+
+int main()
+{
+    Node *root = NULL;
+
+    root = buildTree();
+
+    return 0;
+}
