a1a38646b6d67be4c9f5beb5884dc553ee334da1
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-27 22:11:26 +0530

    Create inorder_traversal.cpp

---
+++ Tree/inorder_traversal.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node *buildTree()
+{
+    int data;
+    cout << "Enter the data: " << endl;
+    cin >> data;
+
+    if (data == -1)
+    {
+        return NULL;
+    }
+
+    // Step 1:
+    Node *root = new Node(data);
+
+    // step 2:
+    cout << "Enter data for left part of " << data << " node " << endl;
+    root->left = buildTree();
+
+    // step 3:
+    cout << "Enter data for right part of " << data << " node " << endl;
+    root->right = buildTree();
+
+    return root;
+}
+
+void inorderTraversal(Node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+
+    // LNR
+    inorderTraversal(root->left);
+    cout << root->data << " ";
+    inorderTraversal(root->right);
+}
+
+int main()
+{
+    Node *root = NULL;
+
+    root = buildTree();
+    // nodes: 20 30 50 -1 60 -1 -1 40 -1 -1 100 -1 -1
+
+    inorderTraversal(root);
+
+    return 0;
+}
