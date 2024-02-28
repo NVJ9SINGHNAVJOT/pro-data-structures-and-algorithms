4a6f31ad7902c6023dd0409bb234c4b7ab12a8c4
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-28 17:36:05 +0530

    Create kth_ancestor.cpp

---
+++ Tree/kth_ancestor.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// algorithm:
+// question: kth ancestor
+// example:
+// answer:
+// explanation:
+// tags:
+
+bool kthAncestor(Node *root, int &k, int p)
+{
+    // base cae
+    if (root == NULL)
+        return false;
+
+    if (root->data == p)
+    {
+        return true;
+    }
+
+    bool leftAns = kthAncestor(root->left, k, p);
+    bool rightAns = kthAncestor(root->right, k, p);
+
+    // wapas aare honge
+    // check left ya right me answer mila ya anhi
+    if (leftAns || rightAns)
+        k--;
+    if (k == 0)
+    {
+        cout << "Answer: " << root->data << endl;
+        k = -1;
+    }
+
+    return leftAns || rightAns;
+}
