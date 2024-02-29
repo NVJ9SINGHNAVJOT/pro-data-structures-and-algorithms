43c4a1027e1cecfde176a4faf4abd45393746b32
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-29 13:07:52 +0530

    Create diagonal_traversal.cpp

---
+++ Tree/diagonal_traversal.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <map>
+using namespace std;
+
+// algorithm:
+// question: diagonal traversal
+// example:
+// answer:
+// explanation:
+// tags:
+
+vector<int> diagonal(Node *root)
+{
+    vector<int> ans;
+    if (root == NULL)
+    {
+        return ans;
+    }
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        // use left element in q
+        Node *temp = q.front();
+        q.pop();
+
+        // go to farmost right direction
+        while (temp)
+        {
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            ans.push_back(temp->data);
+            temp = temp->right;
+        }
+    }
+    return ans;
+}
