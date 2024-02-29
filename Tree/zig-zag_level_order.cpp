6627168c05259cc942284b297aee58d3c191a709
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-02-29 13:28:10 +0530

    Create zig-zag_level_order.cpp

---
+++ Tree/zig-zag_level_order.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <map>
+using namespace std;
+
+// https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// algorithm:
+// question: zig-zag level order
+// example:
+// answer:
+// explanation:
+// tags:
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+vector<vector<int>> zigzagLevelOrder(TreeNode *root)
+{
+    vector<vector<int>> ans;
+    if (!root)
+        return ans;
+
+    bool LtoRDir = true;
+    queue<TreeNode *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        int width = q.size();
+        vector<int> oneLevel(width);
+
+        for (int i = 0; i < width; i++)
+        {
+            TreeNode *front = q.front();
+            q.pop();
+            int index = LtoRDir ? i : width - i - 1;
+            oneLevel[index] = front->val;
+
+            if (front->left)
+            {
+                q.push(front->left);
+            }
+            if (front->right)
+            {
+                q.push(front->right);
+            }
+        }
+
+        // toggle the direction
+        LtoRDir = !LtoRDir;
+        ans.push_back(oneLevel);
+    }
+
+    return ans;
+}
