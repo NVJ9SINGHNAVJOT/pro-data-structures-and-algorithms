14c644dd9d4fe80d3a6b5de4706177bc99d644eb
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-03-03 12:40:42 +0530

    Update spiral_traversal_in_matrix.cpp

--- Array/spiral_traversal_in_matrix.cpp
+++ Array/spiral_traversal_in_matrix.cpp
@@ -14,19 +14,21 @@ using namespace std;
 // explanation:
 // tags:
 
-
-vector<int> spiralOrder(vector<vector<int>>& matrix) {
-    vector<int> ans; // to store the result
-    int n = matrix.size(); // number of rows
-    int m = matrix[0].size(); // number of columns
+vector<int> spiralOrder(vector<vector<int>> &matrix)
+{
+    vector<int> ans;                                      // to store the result
+    int n = matrix.size();                                // number of rows
+    int m = matrix[0].size();                             // number of columns
     int top = 0, left = 0, bottom = n - 1, right = m - 1; // pointers for traversal
-    int totalElements = n * m; 
+    int totalElements = n * m;
     int count = 0;
 
-    while (top <= bottom && left <= right  && totalElements > count) {
+    while (top <= bottom && left <= right && totalElements > count)
+    {
 
         // print the top row from left to right
-        for (int i = left; i <= right && totalElements > count; i++) {
+        for (int i = left; i <= right && totalElements > count; i++)
+        {
             count++;
             ans.push_back(matrix[top][i]);
         }
@@ -34,7 +36,8 @@ vector<int> spiralOrder(vector<vector<int>>& matrix) {
         top++; // increment the top pointer
 
         // print the right column from top to bottom
-        for (int i = top; i <= bottom && totalElements > count; i++) {
+        for (int i = top; i <= bottom && totalElements > count; i++)
+        {
             count++;
             ans.push_back(matrix[i][right]);
         }
@@ -42,36 +45,39 @@ vector<int> spiralOrder(vector<vector<int>>& matrix) {
         right--; // decrement the right pointer
 
         // print the bottom row from right to left
-        for (int i = right; i >= left && totalElements > count; i--) {
+        for (int i = right; i >= left && totalElements > count; i--)
+        {
             count++;
             ans.push_back(matrix[bottom][i]);
         }
 
         bottom--; // decrement the bottom pointer
-        
+
         // print the left column from bottom to top
-        for (int i = bottom; i >= top && totalElements > count; i--) {
+        for (int i = bottom; i >= top && totalElements > count; i--)
+        {
             count++;
             ans.push_back(matrix[i][left]);
         }
 
         left++; // increment the left pointer
-        
     }
     return ans; // return the result
 }
 
-int main() {
-    
+int main()
+{
+
     // sample matrix
-    vector<vector<int>> matrix {{1, 2, 3, 4},
-                                {5, 6, 7, 8},
-                                {9, 10, 11, 12},
-                                {13, 14, 15, 16}};
+    vector<vector<int>> matrix{{1, 2, 3, 4},
+                               {5, 6, 7, 8},
+                               {9, 10, 11, 12},
+                               {13, 14, 15, 16}};
     // call the function
     vector<int> ans = spiralOrder(matrix);
     // print the result
-    for (int x : ans) {
+    for (int x : ans)
+    {
         cout << x << " ";
     }
     cout << endl;
