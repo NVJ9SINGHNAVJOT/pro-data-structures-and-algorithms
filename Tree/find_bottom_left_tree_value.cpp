38756646285cc57a512dd6185220c97190d4eb0f
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-03-03 12:39:28 +0530

    Update find_bottom_left_tree_value.cpp

--- Tree/find_bottom_left_tree_value.cpp
+++ Tree/find_bottom_left_tree_value.cpp
@@ -1,3 +1,10 @@
+#include <iostream>
+#include <queue>
+#include <map>
+#include <set>
+#include <vector>
+using namespace std;
+
 // https://leetcode.com/problems/find-bottom-left-tree-value/description/
 
 // algorithm:
@@ -7,17 +14,16 @@
 // explanation:
 // tags:
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
 class Solution
 {
