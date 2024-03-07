a3d770ceb07070f3e42d4fdc871bbdd347322e5e
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-03-07 15:41:37 +0530

    Create insert_into_bst.cpp

---
+++ Binary Search Tree/insert_into_bst.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *insertIntoBST(Node *root, int data)
+{
+    if (root == NULL)
+    {
+        // this is the first node we have to create
+        root = new Node(data);
+        return root;
+    }
+
+    // check for data comparison with curr node data
+    if (root->data > data)
+    {
+        // insert into left
+        root->left = insertIntoBST(root->left, data);
+    }
+    else
+    {
+        // insert into right
+        root->right = insertIntoBST(root->right, data);
+    }
+    return root;
+}
+
+void takeInput(Node *&root)
+{
+    int data;
+    cin >> data;
+
+    while (data != -1)
+    {
+        root = insertIntoBST(root, data);
+        cin >> data;
+    }
+}
+
+int main()
+{
+    Node *root = NULL;
+    cout << "Enter the data for Node " << endl;
+    takeInput(root);
+    return 0;
+}
