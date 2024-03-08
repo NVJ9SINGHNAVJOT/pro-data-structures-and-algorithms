8b53d9d6772be97d3e138a16fdb1645b9c090561
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-03-08 13:35:39 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+using namespace std;
+
+// algorithm:
+// question:
+// example:
+// answer:
+// explanation:
+// tags:
+
+// https -> link
+
+// Definition for a data structure
+struct DataStucture
+{
+};
+
+void solve()
+{
+}
