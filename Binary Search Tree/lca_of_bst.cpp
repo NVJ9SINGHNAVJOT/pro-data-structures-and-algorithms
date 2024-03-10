e54c9df8ab2dc6d12fced5820d336e221de1769c
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-03-11 00:11:00 +0530

    Create lca_of_bst.cpp

---
+++ Binary Search Tree/lca_of_bst.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+using namespace std;
+
+// algorithm:
+// question: lca of bst
+// example:
+// answer:
+// explanation:
+// tags:
+
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+
+// Definition for a data structure
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+{
+    // base case
+    if (root == NULL)
+        return NULL;
+    // case 1
+    if (p->val < root->val && q->val < root->val)
+    {
+        return lowestCommonAncestor(root->left, p, q);
+    }
+    // case 2
+    else if (p->val > root->val && q->val > root->val)
+    {
+        return lowestCommonAncestor(root->right, p, q);
+    }
+    return root;
+}
