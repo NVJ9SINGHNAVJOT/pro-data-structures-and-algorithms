79e4d97f76a000e0774f50a0303ed233d5c1a314
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-03-24 12:15:37 +0530

    Create kth_smallest_and_greatest_number.cpp

---
+++ Heap/kth_smallest_and_greatest_number.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+using namespace std;
+
+// algorithm:
+// question: kth smallest and greatest number
+// example:
+// answer:
+// explanation:
+// tags:
+
+int getKthGreatestElement(int arr[], int n, int k)
+{
+    // create min heap
+    priority_queue<int, vector<int>, greater<int>> pq;
+    // insert inital k elements of array
+    for (int i = 0; i < k; i++)
+    {
+        pq.push(arr[i]);
+    }
+    // for remaining elements, push only if they are greater than top
+    for (int i = k; i < n; i++)
+    {
+        int element = arr[i];
+        if (element > pq.top())
+        {
+            pq.pop();
+            pq.push(element);
+        }
+    }
+    int ans = pq.top();
+    return ans;
+}
+
+int getKthSmallestElement(int arr[], int n, int k)
+{
+    // create max heap
+    priority_queue<int> pq;
+    // insert inital k elements of array
+    for (int i = 0; i < k; i++)
+    {
+        pq.push(arr[i]);
+    }
+    // for remaining elements, push only if they are less than top
+    for (int i = k; i < n; i++)
+    {
+        int element = arr[i];
+        if (element < pq.top())
+        {
+            pq.pop();
+            pq.push(element);
+        }
+    }
+    int ans = pq.top();
+    return ans;
+}
+
+int main()
+{
+    int arr[] = {10, 5, 20, 4, 15};
+    int n = 5;
+    int k = 5;
+
+    cout << "Kth greatest : Ans is: " << getKthGreatestElement(arr, n, k) << endl;
+    cout << "Kth smallest : Ans is: " << getKthSmallestElement(arr, n, k) << endl;
+    cout << "Kth greatest : Ans is: " << getKthGreatestElement(arr, n, 1) << endl;
+    cout << "Kth smallest : Ans is: " << getKthSmallestElement(arr, n, 1) << endl;
+    return 0;
+}
