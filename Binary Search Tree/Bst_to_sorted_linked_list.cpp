3bbc2b7cca354b0a26d96569fc6266f23a98dfa9
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-07-25 20:15:46 +0530

    Create Bst_to_sorted_linked_list.cpp

---
+++ Binary Search Tree/Bst_to_sorted_linked_list.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <list>
+#include <unordered_set>
+#include "../includes/tree.cpp"
+using namespace std;
+
+// algorithm:
+// question: bst to sorted linked list
+// example:
+// answer:
+// explanation:
+// tags:
+
+void inorder(Node *root, Node *&prev)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inorder(root->left, prev);
+    prev->left = NULL;
+    prev->right = root;
+    prev = root;
+    inorder(root->right, prev);
+}
+
+Node *flatten(Node *root)
+{
+    Node *dummy = new Node(-1);
+    Node *prev = dummy;
+    inorder(root, prev);
+    prev->left = prev->right = NULL;
+    root = dummy->right;
+    return root;
+}
