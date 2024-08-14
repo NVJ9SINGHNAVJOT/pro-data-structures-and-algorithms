673c4566946b0274997187c91150646d7d6fdd57
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-08-14 14:16:33 +0530

    Create merge_two_bst_to_from_one_bst.cpp

---
+++ Binary Search Tree/merge_two_bst_to_from_one_bst.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <list>
+#include <unordered_set>
+#include <numeric>
+#include "../includes/tree.cpp"
+using namespace std;
+
+// algorithm:
+// question: merge two bst to from one bst
+// example:
+// answer:
+// explanation:
+// tags:
+
+vector<int> merge(Node *root1, Node *root2)
+{
+    vector<int> ans;
+    stack<Node *> sa, sb;
+    Node *a = root1, *b = root2;
+
+    while (a || b || !sa.empty() || !sb.empty())
+    {
+        while (a)
+        {
+            sa.push(a);
+        }
+        while (b)
+        {
+            sb.push(b);
+        }
+        if (sb.empty() || (!sa.empty() && sa.top()->data <= sb.top()->data))
+        {
+            auto atop = sa.top();
+            ans.push_back(atop->data);
+            sa.pop();
+            a = atop->right;
+        }
+        else
+        {
+            auto btop = sb.top();
+            sb.pop();
+            ans.push_back(btop->data);
+            b = btop->right;
+        }
+    }
+    return ans;
+}
