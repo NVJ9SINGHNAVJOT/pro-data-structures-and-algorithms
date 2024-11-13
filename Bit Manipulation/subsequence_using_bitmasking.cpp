937177b5c9ef27ef6fb90b543e5d3036bb22caf3
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-11-13 22:15:02 +0530

    feat(question): :art: updated

---
+++ Bit Manipulation/subsequence_using_bitmasking.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <list>
+#include <unordered_set>
+#include <numeric>
+using namespace std;
+
+void getSubsequences(string str)
+{
+    vector<string> ans;
+    int n = str.length();
+
+    for (int num = 0; num < (1 << n); num++)
+    {
+        string temp = "";
+        // we will create subsequence string in this temp string
+        for (int i = 0; i < n; i++)
+        {
+            char ch = str[i];
+            if (num & (1 << i))
+            {
+                // non-zero value k liye , include current character
+                temp.push_back(ch);
+            }
+        }
+
+        if (temp.length() >= 0)
+        {
+            // store in ans
+            ans.push_back(temp);
+        }
+    }
+    sort(ans.begin(), ans.end());
+    cout << "Count of subsequences: " << ans.size() << endl;
+    cout << "printing the subsequences: " << endl;
+    for (auto i : ans)
+    {
+        cout << i << endl;
+    }
+}
+
+int main()
+{
+    string str = "abc";
+    getSubsequences(str);
+    return 0;
+}
