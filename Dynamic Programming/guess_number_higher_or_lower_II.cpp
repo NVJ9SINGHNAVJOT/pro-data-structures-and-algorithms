d1a0c604bfff0794f46a0b30e858ed059685ffe6
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-11-15 00:20:35 +0530

    feat(question): :sparkles: updated

--- Dynamic Programming/guess_number_higher_or_lower_II.cpp
+++ Dynamic Programming/guess_number_higher_or_lower_II.cpp
@@ -10,6 +10,7 @@
 #include <list>
 #include <unordered_set>
 #include <numeric>
+#include "../includes/vector.cpp"
 using namespace std;
 
 // algorithm:
@@ -50,11 +51,36 @@ int solveMemo(int start, int end, vector<vector<int>> &dp)
     int ans = INT_MAX;
     for (int i = start; i <= end; i++)
     {
-        ans = min(ans, i + max(solveRec(start, i - 1), solveRec(i + 1, end)));
+        ans = min(ans, i + max(solveMemo(start, i - 1, dp), solveMemo(i + 1, end, dp)));
     }
     dp[start][end] = ans;
 
     return ans;
+}
+
+int solveTab(int n)
+{
+    vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+
+    for (int start = n; start >= 1; start--)
+    {
+        for (int end = 1; end <= n; end++)
+        {
+
+            if (start >= end)
+                continue;
+            else
+            {
+                int ans = INT_MAX;
+                for (int i = start; i <= end; i++)
+                {
+                    ans = min(ans, i + max(dp[start][i - 1], dp[i + 1][end]));
+                }
+                dp[start][end] = ans;
+            }
+        }
+    }
+    return dp[1][n];
 }
 
 int getMoneyAmount(int n)
@@ -63,7 +89,8 @@ int getMoneyAmount(int n)
     // ans = solveRec(1, n);
 
     vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-    ans = solveMemo(1, n, dp);
+
+    ans = solveTab(n);
     return ans;
 }
 
