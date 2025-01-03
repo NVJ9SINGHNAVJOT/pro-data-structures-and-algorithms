23ce8b43e92f90accfdd69704d5e2419d570b5d4
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2025-01-03 20:41:31 +0530

    feat(question): added

---
+++ HashMap & Trie/trie.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <list>
+#include <unordered_set>
+#include <numeric>
+using namespace std;
+
+class TrieNode
+{
+public:
+    char data;
+    TrieNode *children[26] = {};
+    bool isTerminal;
+
+    TrieNode(char d)
+    {
+        this->data = d;
+        this->isTerminal = false;
+    }
+};
+
+class Trie
+{
+    TrieNode *root = new TrieNode('-');
+    void insertWordHelper(TrieNode *root, string &word, int &i)
+    {
+        // base case
+        if (i >= word.length())
+        {
+            root->isTerminal = true;
+            return;
+        }
+
+        char ch = word[i++];
+        int index = ch - 'a';
+        TrieNode *child;
+
+        // present
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else
+        {
+            // not present
+            child = new TrieNode(ch);
+            root->children[index] = child;
+        }
+
+        // recursion sambhal lega
+        insertWordHelper(child, word, i);
+    }
+
+    bool searchWordHelper(TrieNode *root, string &word, int &i)
+    {
+        // base case
+        if (i == word.length())
+        {
+            return root->isTerminal;
+        }
+
+        char ch = word[i++];
+        int index = ch - 'a';
+        TrieNode *child;
+
+        // present
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else
+        {
+            return false;
+        }
+
+        // rec call
+        return searchWordHelper(child, word, i);
+    }
+
+public:
+    void insertWord(string word)
+    {
+        int i = 0;
+        this->insertWordHelper(this->root, word, i);
+    }
+    bool searchWordHelper(string word)
+    {
+        int i = 0;
+        return this->searchWordHelper(this->root, word, i);
+    }
+};
+
+int main()
+{
+}
